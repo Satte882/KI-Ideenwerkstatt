290fef9b51782a2d5bcfcedfc4a25f2c6280cfa5
Satte882 <satinder.sandhoo@gmx.de>
2026-09-21 18:46:08 +0200

    VS1/3: keep process workspace independent of accelerator runtime

--- ki_radar/architecture/views.py
+++ ki_radar/architecture/views.py
@@ -391,13 +391,11 @@ def process_analysis_detail(request, pk):
         .order_by("-revision")
         .first()
     )
-    investigation_effective_budget = None
-    if current_investigation_snapshot is not None:
-        from ki_radar.accelerator.investigation_runtime import budget_from_snapshot
-
-        investigation_effective_budget = budget_from_snapshot(
-            current_investigation_snapshot.run_limits
-        )
+    investigation_effective_budget = (
+        dict(current_investigation_snapshot.run_limits)
+        if current_investigation_snapshot is not None
+        else None
+    )
 
     return render(
         request,
