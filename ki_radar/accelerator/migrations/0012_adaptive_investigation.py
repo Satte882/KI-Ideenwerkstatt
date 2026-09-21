77d5b49f618a9577084993caaf834493f6cc2bb1
Satte882 <satinder.sandhoo@gmx.de>
2026-09-21 16:21:17 +0200

    VS1/2: include source relevance in migration

--- ki_radar/accelerator/migrations/0012_adaptive_investigation.py
+++ ki_radar/accelerator/migrations/0012_adaptive_investigation.py
@@ -49,6 +49,7 @@ class Migration(migrations.Migration):
                 ("usage", models.JSONField(default=dict)),
                 ("execution_snapshot", models.JSONField(default=dict)),
                 ("claim_register", models.JSONField(default=list)),
+                ("source_relevance", models.JSONField(default=dict)),
                 ("register_revision", models.PositiveIntegerField(default=1)),
                 ("register_hash", models.CharField(max_length=64)),
                 ("brief_payload", models.JSONField(default=dict)),
